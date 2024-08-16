325066596cbf4580f63c91528e104bc923293ac4
REMR11 <remr11@ufg.edu.sv>
2024-08-15 22:12:05 -0600

    feat: Creacion de DBContext * Falta conexion a base de datos * falta configurar FluentApi

--- Restaurante.DAL/RestauranteDBContext.cs
+++ Restaurante.DAL/RestauranteDBContext.cs
@@ -1,8 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurante.EL;
 using System;
 
 namespace Restaurante.DAL;
 
-public class RestauranteDBContext
+public class RestauranteDBContext : DbContext
 {
-    
+    public RestauranteDBContext(){}
+    public RestauranteDBContext(DbContextOptions<RestauranteDBContext> options) : base(options) { }
+
+    public DbSet<Ingredientes> Ingredientes { get; set; }
+    public DbSet<Plantillo>plantillos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //modelBuilder.Entity<>
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        if (!optionsBuilder.IsConfigured) {
+            optionsBuilder.UseSqlServer(@"");
+        }
+    }
+
+
+
 }
--- Restaurante.EL/obj/Debug/net8.0/Restaurante.EL.AssemblyInfo.cs
+++ Restaurante.EL/obj/Debug/net8.0/Restaurante.EL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restaurante.EL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5f64c70cc6aeca7257a49d55db4c540e46a362")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restaurante.EL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restaurante.EL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
